c225aff97910429de6f7b620e5285da41da94671
zhang liang <lyszhang@126.com>
2020-11-20 15:13:21 +0800

    benchmark 测试

--- equation/equation_test.go
+++ equation/equation_test.go
@@ -7,6 +7,7 @@
 package equation
 
 import (
+	"github.com/dengsgo/math-engine/engine"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -15,4 +16,12 @@ func TestNew(t *testing.T) {
 	buf := "z=x+y"
 	_, err := New(buf)
 	assert.NoError(t, err)
+}
+
+func BenchmarkExecEquation(b *testing.B) {
+	engine.RegFunction("COMPARE", 2, engine.Compare)
+	es := []Equation{{"COMPARE(x, y)", "z"}}
+	for i := 0; i < b.N; i++ {
+		ExecEquation(es)
+	}
 }
